534a8ab42e22fffe64dfa7ece9fe13bc66e8c673
keguangzhang <kez013@ucsd.edu>
2023-01-26 10:33:00 -0800

    summary
    summary

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,19 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[] {5, 4, 3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input = {1, 2, 3, 4, 5, 6, 7};
+    assertArrayEquals(new int[] {7, 6, 5, 4, 3, 2, 1}, 
+            ArrayExamples.reversed(input));
+            
+  }
 }
